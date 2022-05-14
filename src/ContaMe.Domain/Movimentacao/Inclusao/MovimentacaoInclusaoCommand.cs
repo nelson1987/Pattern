70e168e3bfefa79e72ed36ab7fd9f0ef27a08c0c
nelson1987 <nelson.ash@outlook.com>
2022-05-14 19:00:22 -0300

    Inclusao de Classes base

---
+++ src/ContaMe.Domain/Movimentacao/Inclusao/MovimentacaoInclusaoCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace ContaMe.Domain.Movimentacao.Inclusao
+{
+    public class MovimentacaoInclusaoCommand
+    {
+    }
+    public class MovimentacaoInclusaoCommandValidator
+    {
+    }
+    public class MovimentacaoInclusaoCommandHandler
+    {
+    }
+    public class MovimentacaoInclusaoCommandModel
+    {
+    }
+    public class MovimentacaoInclusaoCommandResponse
+    {
+    }
+}
